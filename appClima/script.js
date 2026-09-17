65dd16309b16bef8241e1503eb5fa823c08213cb
MateusSO310709 <mateus.oliveira310709@gmail.com>
2026-09-17 20:13:30 -0300

    17/09/26 geocodificação no app de clima

--- appClima/script.js
+++ appClima/script.js
@@ -4,6 +4,8 @@ const botaoBuscar = document.getElementById('buscar');
 
 const resultado = document.getElementById('resultado');
 
+const localizacao = document.getElementById('localizacao');
+
 botaoBuscar.addEventListener("click", async function() {    
     const resposta = await fetch(`https://geocoding-api.open-meteo.com/v1/search?name=${cidade.value}&count=1&language=pt&format=json`);
 
@@ -30,8 +32,34 @@ botaoBuscar.addEventListener("click", async function() {
 
     console.log(`Latitude: ${latitude}`);
     console.log(`Longitude: ${longitude}`);
-    console.log(`Temperatura: ${clima}`)
+    console.log(`Temperatura: ${temperatura}`)
     resultado.innerHTML += `<p>Temperatura: ${temperatura} °C</p>`
     resultado.innerHTML += `<p>Umidade: ${umidade} %</p>`
     resultado.innerHTML += `<p>Vento: ${vento} km/h</p>`
-})
+});
+
+localizacao.addEventListener("click", function() {
+    navigator.geolocation.getCurrentPosition(async function(posicao) {
+        const latitude = posicao.coords.latitude;
+        const longitude = posicao.coords.longitude;
+
+        const respostaClima = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`);
+        const dadosClima = await respostaClima.json();
+        console.log(dadosClima);
+
+        const respostaLocal = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=json&accept-language=pt-BR`);
+        const dadosLocal = await respostaLocal.json();
+        console.log(dadosLocal);
+    
+        const cidade = dadosLocal.adress.city
+
+        const temperatura = dadosClima.current.temperature_2m;
+        const umidade = dadosClima.current.relative_humidity_2m;
+        const vento = dadosClima.current.wind_speed_10m;
+
+        resultado.innerHTML = `<h2 id="clima">Clima de ${cidade}</h2>`
+        resultado.innerHTML = `<p>Temperatura: ${temperatura} °C</p>`
+        resultado.innerHTML += `<p>Umidade: ${umidade} %</p>`
+        resultado.innerHTML += `<p>Vento: ${vento} km/h</p>`
+    });
+});
